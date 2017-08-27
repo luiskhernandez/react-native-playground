9a47a66acbf75e8df13ce459bc6da0528de6a1c9
Luis Hernandez <luisk.hernandez.macias@gmail.com>
2017-08-27 10:23:42 -0500

    Touchables

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Alert, Button, StyleSheet, Text, View, Image, TextInput } from 'react-native';
+import Touchables from './components/touchables.js'
 
 class Blink extends Component {
   constructor(props) {
@@ -41,42 +42,8 @@ export default class App extends React.Component {
     let pic = {
       uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
     };
-    return (
-      <View style={{flex: 1}}>
-        <View style={{flex: 3, backgroundColor: 'powderblue', flexDirection: 'column', paddingTop: 40}} >
-          <TextInput
-            value={this.state.text}
-            style={{height: 40}}
-            placeholder="Type here to translate!"
-            onChangeText={(text) => this.setState({text})}
-          />
-          <Text style={{padding: 10, fontSize: 42}}>
-            {this.state.text.split(' ').map((word) => word && '🍕').join(' ')}
-          </Text>
-        </View>
-        <View style={{flex: 1, backgroundColor: 'skyblue'}}>
-          <Button
-            onPress={() => { Alert.alert('You tapped the button!')}}
-            title="Press Me"
-          />
-        </View>
-          <View style={{flex: 3, backgroundColor: 'steelblue', flexDirection: 'row', justifyContent: 'center', alignItems: 'center'}}>
-          <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
-          <View style={{width: 50, height: 50, backgroundColor: 'skyblue'}} />
-          <View style={{width: 50, height: 50, backgroundColor: 'red'}} />
-        </View>
-        <View style={{flex: 3, backgroundColor: 'black', flexDirection: 'column', justifyContent: 'center', alignItems: 'center'}}>
-          <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
-          <View style={{width: 50, height: 50, backgroundColor: 'skyblue'}} />
-          <View style={{width: 50, height: 50, backgroundColor: 'red'}} />
-        </View>
-        <View style={{flex: 3, backgroundColor: 'blue', flexDirection: 'row', justifyContent: 'flex-end', alignItems: 'flex-start'}}>
-          <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
-          <View style={{width: 50, height: 50, backgroundColor: 'skyblue'}} />
-          <View style={{width: 50, height: 50, backgroundColor: 'red'}} />
-        </View>
-      </View>
-    );
+
+    return <Touchables />
   }
 }
 
